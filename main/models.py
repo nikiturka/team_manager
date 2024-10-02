fd048ec4eb32c817c8297958bedd625eabe03e85
Nikita Turda <turda.nikita.bsn@gmail.com>
2024-10-02 20:25:36 +0300

    create models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    teams = models.ManyToManyField(Team, related_name='members')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
